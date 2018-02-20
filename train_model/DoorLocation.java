b3c8027d7e75cbb60086227d3635e33712d574c2
Ryan Matthews <ryanmatthews@pitt.edu>
2018-02-20 15:14:02 -0500

    DoorLocation

---
+++ train_model/DoorLocation.java
@@ -0,0 +1,16 @@
+/*
+ * Ryan Matthews
+ *
+ * Simulation and communication
+ */
+
+package train_model;
+
+// Door location on a train
+//
+// Each location may refer to a single door or a group of doors.
+public enum DoorLocation
+{
+	left,
+	right
+}
