5ff75b9a00fcb4a14c316d97c8a358ede6a5ce50
sapike <sap117@pitt.edu>
2018-02-20 19:58:04 -0500

    Adding ctc files
    They talk to each other, hooray!

---
+++ ctc/CtcDriver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myctc;
+
+/**
+ *
+ * @author missm
+ */
+public class CtcDriver {
+    
+    public static void main(String[] args)
+    {
+        MyCtc ctc = new MyCtc();
+    }
+    
+}
