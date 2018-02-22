e3d1d984071b5c812a588e512ab78905ee746863
Ryan Matthews <ryanmatthews@pitt.edu>
2018-02-22 11:31:34 -0500

    PointHeat

---
+++ train_model/PointHeat.java
@@ -0,0 +1,47 @@
+/*
+ * Ryan Matthews
+ *
+ * Simulation
+ */
+
+package train_model;
+
+// Physics object with thermal energy
+//
+// Has a "heat inertia".
+//
+// Maintains temperature.
+
+public class PointHeat
+{
+	// Joule per celsius
+	private final double heatInertia = 1.0;
+
+	private double temperature = 20.0;
+
+	// Returns the current temperature.
+	//
+	// Units: Celsius
+	public double temperature()
+	{
+		return temperature;
+	}
+
+	// Simulates heating/cooling with a constant power for an amount of
+	// time.
+	//
+	// Updates temperature.
+	//
+	// Does the math and returns immediately, i.e., does not sleep.
+	//
+	// Parameter power is Watts transferred in. Can be negative.
+	//
+	// Parameter time is milliseconds.
+	public void conduct(double power, int time)
+	{
+		// joules
+		double energy = power * time / 1000.0;
+
+		temperature += energy / heatInertia;
+	}
+}
