ae90fb6ea9bf966127c5821734e4cf05a815b007
Ryan Matthews <ryanmatthews@pitt.edu>
2018-03-05 21:16:27 -0500

    Updater updates at fixed frequency

--- updater/Updateable.java
+++ updater/Updateable.java
@@ -10,6 +10,9 @@ public interface Updateable
 {
 	// Updates this object.
 	//
+	// Parameter time is the amount of time that this update accounts for,
+	// i.e., the amount of simulated time that passes during the cycle.
+	//
 	// Shall not block. Shall be fast.
-	public void update();
+	public void update(int time);
 }
--- updater/Updater.java
+++ updater/Updater.java
@@ -8,14 +8,14 @@ package updater;
 
 // Module iteration scheduler
 //
-// Updates modules in round-robin fashion.
+// Updates modules in round-robin-ish fashion.
 
 public class Updater
 {
-	private Updateable[] objects;
+	// milliseconds
+	private static int period = 100;
 
-	// index of next object to update
-	private int next = 0;
+	private Updateable[] objects;
 
 	// Constructs an Updater which updates a list of objects in order.
 	public Updater(Updateable[] objects)
@@ -26,8 +26,7 @@ public class Updater
 	// Runs a single scheduler iteration.
 	public void iteration()
 	{
-		objects[next].update();
-
-		next = (next + 1) % objects.length;
+		for (Updateable i : objects)
+			i.update(period);
 	}
 }
