a5c85d0a696e67cf571ce339ff34db66dc7531d1
Ryan Matthews <ryanmatthews@pitt.edu>
2018-03-07 15:13:14 -0500

    Updater scheduleAtFixedRate()

--- updater/Updater.java
+++ updater/Updater.java
@@ -6,6 +6,9 @@
 
 package updater;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 // Module iteration scheduler
 //
 // Updates modules in round-robin-ish fashion.
@@ -17,6 +20,9 @@ public class Updater
 
 	private Updateable[] objects;
 
+	private Timer timer = new Timer();
+	private TimerTask task = new Task(this);
+
 	// Constructs an Updater which updates a list of objects in order.
 	public Updater(Updateable[] objects)
 	{
@@ -24,9 +30,37 @@ public class Updater
 	}
 
 	// Runs a single scheduler iteration.
+	//
+	// NOTE: Not thread-safe if scheduleAtFixedRate() has already been
+	// called.
 	public void iteration()
 	{
 		for (Updateable i : objects)
 			i.update(period);
+	}
+
+	// Schedules iteration() for repeated fixed-rate execution, beginning
+	// immediately.
+	//
+	// Parameter period is the time in milliseconds between iterations.
+	public void scheduleAtFixedRate(int period)
+	{
+		timer.scheduleAtFixedRate(task, 0, period);
+	}
+
+	// TimerTask that calls iteration() on an Updater.
+	private class Task extends TimerTask
+	{
+		Updater updater;
+
+		public Task(Updater updater)
+		{
+			this.updater = updater;
+		}
+
+		public void run()
+		{
+			updater.iteration();
+		}
 	}
 }
