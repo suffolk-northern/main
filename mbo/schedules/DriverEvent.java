93d488499b078d92beda8a9759ffc85c6986dbf2
violasox <viola.sox@gmail.com>
2018-04-01 21:24:31 -0400

    Implemented TrainEvent and DriverEvent

--- mbo/schedules/DriverEvent.java
+++ mbo/schedules/DriverEvent.java
@@ -1,9 +1,33 @@
 package mbo.schedules;
 
+import java.sql.Time;
+
 /**
  *
  * @author Kaylene Stocking
  */
 public class DriverEvent {
+	private Time eventTime;
+	private EventType event;
 	
+	public DriverEvent(Time t, EventType e)
+	{
+		eventTime = t;
+		event = e;
+	}
+	
+	public enum EventType
+	{
+		EMBARK, DISEMBARK
+	}
+	
+	public Time getTime()
+	{
+		return eventTime;
+	}
+	
+	public EventType getEvent()
+	{
+		return event;
+	}
 }
