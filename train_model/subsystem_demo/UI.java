0ea3089d83655a1ac353caf4724037d92742b412
Ryan Matthews <ryanmatthews@pitt.edu>
2018-04-03 14:51:23 -0400

    Train model demo UI window close behavior
    Does not exit the application.
    
    Isn't the right behavior for the subsystem demo, but works for
    integration.Main.

--- train_model/subsystem_demo/UI.java
+++ train_model/subsystem_demo/UI.java
@@ -208,7 +208,7 @@ public class UI
         mboRxSpeed = new javax.swing.JLabel();
         mboRxAuthority = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel1.setText("Control");
