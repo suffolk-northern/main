20d71a2caef553d2f7c394521b6c7aa3295d19ce
Roger Xue <rxue14@gmail.com>
2018-04-11 21:07:56 -0400

    Set yard block prev

--- track_model/TrackModel.java
+++ track_model/TrackModel.java
@@ -359,9 +359,13 @@ public class TrackModel implements Updateable {
 				for (int j = 0; j < tbs.size(); j++) {
 					TrackBlock switchBlock = tbs.get(j);
 					if (newSwitch.switchBlockId == switchBlock.block
-							&& newSwitch.line.equalsIgnoreCase(switchBlock.line)
-							&& switchBlock.block != 0) {
-						switchBlock.nextBlockId = newSwitch.block;
+							&& newSwitch.line.equalsIgnoreCase(switchBlock.line)) {
+						System.out.println(switchBlock.block);
+						if (switchBlock.block == 0) {
+							switchBlock.prevBlockId = newSwitch.block;
+						} else {
+							switchBlock.nextBlockId = newSwitch.block;
+						}
 					}
 				}
 			}
