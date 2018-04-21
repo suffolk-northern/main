c5bec4bf3132ebfdd194801ca44d1fa0d92e9780
Ryan Matthews <ryanmatthews@pitt.edu>
2018-04-21 00:49:35 -0400

    Netbeans GUI passengers

--- train_model/subsystem_demo/UI.java
+++ train_model/subsystem_demo/UI.java
@@ -185,6 +185,10 @@ public class UI
         cabinLights = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
         cabinHeater = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        cabinPassengers = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        cabinCrew = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jPanel9 = new javax.swing.JPanel();
         jLabel20 = new javax.swing.JLabel();
@@ -213,7 +217,7 @@ public class UI
         mboRxSpeed = new javax.swing.JLabel();
         mboRxAuthority = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel1.setText("Control");
@@ -396,7 +400,7 @@ public class UI
                 .addComponent(jLabel8)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(24, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jLabel15.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
@@ -428,6 +432,18 @@ public class UI
         cabinHeater.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         cabinHeater.setText("OFF");
 
+        jLabel5.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel5.setText("Pass");
+
+        cabinPassengers.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        cabinPassengers.setText("999");
+
+        jLabel18.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel18.setText("Crew");
+
+        cabinCrew.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        cabinCrew.setText("9");
+
         javax.swing.GroupLayout jPanel8Layout = new javax.swing.GroupLayout(jPanel8);
         jPanel8.setLayout(jPanel8Layout);
         jPanel8Layout.setHorizontalGroup(
@@ -438,9 +454,13 @@ public class UI
                     .addComponent(jLabel16)
                     .addComponent(jLabel17)
                     .addComponent(jLabel10)
-                    .addComponent(jLabel14))
+                    .addComponent(jLabel14)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel18))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(cabinCrew)
+                    .addComponent(cabinPassengers)
                     .addComponent(cabinHeater)
                     .addComponent(cabinLights)
                     .addComponent(cabinRDoors)
@@ -466,7 +486,15 @@ public class UI
                 .addGroup(jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel14)
                     .addComponent(cabinHeater))
-                .addContainerGap(27, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(cabinPassengers))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel18)
+                    .addComponent(cabinCrew))
+                .addContainerGap(32, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel7Layout = new javax.swing.GroupLayout(jPanel7);
@@ -485,8 +513,8 @@ public class UI
                 .addContainerGap()
                 .addComponent(jLabel15)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jPanel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -841,9 +869,11 @@ public class UI
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel beaconRxString;
+    private javax.swing.JLabel cabinCrew;
     private javax.swing.JLabel cabinHeater;
     private javax.swing.JLabel cabinLDoors;
     private javax.swing.JLabel cabinLights;
+    private javax.swing.JLabel cabinPassengers;
     private javax.swing.JLabel cabinRDoors;
     private javax.swing.JLabel controlPower;
     private javax.swing.JLabel controlSBrake;
@@ -855,6 +885,7 @@ public class UI
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel20;
@@ -870,6 +901,7 @@ public class UI
     private javax.swing.JLabel jLabel34;
     private javax.swing.JLabel jLabel35;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
